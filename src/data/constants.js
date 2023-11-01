2b230455921f9f55c74533464c49318aae11e2d9
shaliniprasad33 <149572220+shaliniprasad33@users.noreply.github.com>
2023-11-01 14:57:11 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -20,7 +20,7 @@ export const projects = [
   },
   {
     id: 1,
-    title: "Banners for Grocery!!!!!!!!",
+    title: "Banners for Grocery",
     date: "Sept 2022",
     description:
       "🛒 Presenting our latest masterpiece - the eye-catching banner for our grocery app UI and design! 📱🌟 We've poured our creativity into this design to make your grocery shopping experience better than ever. The banner captures the essence of convenience, quality, and variety that our app offers. Stay tuned for more updates and sneak peeks of our design journey. 🛍️ #GroceryApp #UIBanner #DesignInspiration #GroceryShopping",
@@ -52,30 +52,8 @@ export const projects = [
     category: "graphics",
     webapp: "https://www.behance.net/gallery/183554611/FYNDORA-GYM",
   },
-  {
-    id: 7,
-    title: "Quiz App",
-    date: "Dec 2020 - Jan 2021",
-    description:
-      "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
-    image:
-      "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
-    tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
-    category: "video",
-    webapp: "https://github.com/rishavchanda/Quiz-Earn",
-  },
-  {
-    id: 8,
-    title: "Face Recognition",
-    date: "Jan 2021",
-    description:
-      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-    image:
-      "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
-    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-    category: "uiux",
-    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  },
+ 
+  
 ];
 export const Bio = {
   name: "Shalini Prasad",
